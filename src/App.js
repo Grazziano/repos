5013c87e411d1592aeea6a0c373c00f993cef0da
Grazziano Fagundes <grazzianofagundes@gmail.com>
2023-09-04 17:42:17 -0300

    Criando estilos

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from 'react';
 import RoutesApp from './routes';
+import GlobalStyle from './styles/global';
 
 function App() {
   return (
-    <RoutesApp />
+    <>
+      <GlobalStyle />
+      <RoutesApp />
+    </>
   );
 }
 
