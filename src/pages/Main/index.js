a3a591d22bb58044bc37b94da9d4f8be21e6fa61
Grazziano Fagundes <grazzianofagundes@gmail.com>
2023-09-04 17:55:03 -0300

    Estilizando página main

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,10 +1,21 @@
 import React from 'react';
-import { Title } from './styles';
+import { FaGithub, FaPlus } from 'react-icons/fa';
+import { Container, Form, SubmitButton } from './styles';
 
 export default function Main() {
   return (
-    <div>
-      <Title>Main</Title>
-    </div>
+    <Container>
+      <h1>
+        <FaGithub size={25} /> Meus Repositórios
+      </h1>
+
+      <Form onSubmit={() => {}}>
+        <input type="text" placeholder="Adicionar Repositórios" />
+
+        <SubmitButton>
+          <FaPlus color="#FFF" size={14} />
+        </SubmitButton>
+      </Form>
+    </Container>
   );
 }
