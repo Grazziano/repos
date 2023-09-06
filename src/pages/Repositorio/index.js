d5d7cf52cbe4539638da2945351a6bc0d8135b24
Grazziano Fagundes <grazzianofagundes@gmail.com>
2023-09-06 13:28:26 -0300

    Detalhes do repositório

--- src/pages/Repositorio/index.js
+++ src/pages/Repositorio/index.js
@@ -1,9 +1,14 @@
 import React from 'react';
+import { useParams } from 'react-router-dom';
 
 export default function Repositorio() {
+  const { repositorio } = useParams();
+
   return (
     <div>
-      <h1>Repositorio</h1>
+      <h1 style={{ color: '#FFF' }}>
+        Repositorio {decodeURIComponent(repositorio)}
+      </h1>
     </div>
   );
 }
