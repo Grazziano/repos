65b182e59eaf8d3e79f4a848e94203636a6c809d
Grazziano Fagundes <grazzianofagundes@gmail.com>
2023-09-06 13:46:09 -0300

    Carregando detalhes

--- src/pages/Repositorio/index.js
+++ src/pages/Repositorio/index.js
@@ -1,14 +1,39 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
+import { Container } from './styles';
+import api from '../../services/api';
 
 export default function Repositorio() {
-  const { repositorio } = useParams();
+  const { repositorio } = useParams({});
 
-  return (
-    <div>
-      <h1 style={{ color: '#FFF' }}>
-        Repositorio {decodeURIComponent(repositorio)}
-      </h1>
-    </div>
-  );
+  const [repo, setRepo] = useState({});
+  const [issues, setIssues] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    async function load() {
+      const nomeRepo = decodeURIComponent(repositorio);
+
+      const [repositorioData, issuesData] = await Promise.all([
+        api.get(`/repos/${nomeRepo}`),
+        api.get(`/repos/${nomeRepo}/issues`, {
+          params: {
+            state: 'open',
+            per_page: 5,
+          },
+        }),
+      ]);
+
+      // console.log(repositorioData.data);
+      // console.log(issuesData.data);
+
+      setRepo(repositorioData.data);
+      setIssues(issuesData.data);
+      setLoading(false);
+    }
+
+    load();
+  }, [repositorio]);
+
+  return <Container></Container>;
 }
